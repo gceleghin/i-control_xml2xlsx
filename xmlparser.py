b31602020ea4e25426839a81ac34129807d0dea6
Giordano Celeghin <giordano.celeghin@gmail.com>
2022-09-06 15:25:13 +0200

    Added cycle support

--- xmlparser.py
+++ xmlparser.py
@@ -41,35 +41,37 @@ duplicate_index = 0 # For when we have name duplicates we need to sort
 # The XML output already has the Excel positions (EG: "A1")
 # it wants the results to be in so we keep track of
 # the highest row it writes, so we can safely write after that
+
 for section in root.iter('Section'):
     highestrow = 0
 
+    worksheet_name = section.get('Name')[:24] # Worksheets' names can't be longer than 31
+
+    for dataset in section.iter('Data'):
+        cycle = dataset.attrib["Cycle"]
+        worksheet_name_cycle = worksheet_name + "_cycle_" + str(cycle)
+        try:
+            worksheet = workbook.add_worksheet(worksheet_name_cycle)
+        except xlsxwriter.exceptions.DuplicateWorksheetName:
+            print("Worksheet '" + worksheet_name_cycle + "' already existing, renaming")
+            worksheet_name_cycle += str(duplicate_index)
+            print("Worksheet renamed to " + worksheet_name_cycle)
+            duplicate_index += 1
+            worksheet = workbook.add_worksheet(worksheet_name_cycle)
+        for well in dataset.iter('Well'):
+            position = well.get('Pos')
+            # We extract the row's number from the Excel position string
+            # and convert it to the (0,0) notation
+            pos_row = (int(re.search(r'\d+', position).group()) - 1)
+            # TODO: Change the locale instead of brutally change commas into dots
+            value = float((well.find('Single').text).replace(',','.'))
+            worksheet.write_number(position, value, well_format)
+
+    # Section parameters go into their own worksheet after the cycles
+    worksheet = workbook.add_worksheet(worksheet_name + "_param")
+    highestrow = 0
     timestart = section.find('Time_Start').text
     timeend = section.find('Time_End').text
-    worksheet_name = section.get('Name')[:30] # Worksheets' names can't be longer than 31
-    try:
-        worksheet = workbook.add_worksheet(worksheet_name)
-    except xlsxwriter.exceptions.DuplicateWorksheetName:
-        print("Worksheet '" + worksheet_name + "' already existing, renaming")
-        worksheet_name += str(duplicate_index)
-        print("Worksheet renamed to " + worksheet_name)
-        duplicate_index += 1
-        worksheet = workbook.add_worksheet(worksheet_name)
-    # Results are divided in "wells"
-    for well in section.iter('Well'):
-        position = well.get('Pos')
-        # We extract the row's number from the Excel position string
-        # and convert it to the (0,0) notation
-        pos_row = (int(re.search(r'\d+', position).group()) - 1)
-        if (highestrow < pos_row): # Keep track of the highest row
-            highestrow = pos_row
-        # TODO: Change the locale instead of brutally change commas into dots
-        value = float((well.find('Single').text).replace(',','.'))
-        worksheet.write_number(position, value, well_format)
-
-    # Parameters go into their own worksheet
-    worksheet = workbook.add_worksheet("Parameters")
-    highestrow = 0
     worksheet.write(highestrow, 0, "Time start:", tag_format)
     worksheet.write(highestrow, 1, timestart)
     highestrow += 1
